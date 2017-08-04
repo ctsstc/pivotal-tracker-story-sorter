16e92cda7172704134e39caeeebda2891b436755
Cody Swartz <ctsstc@gmail.com>
2017-08-04 16:25:28 -0700

    Gets all stories per story states

--- main.rb
+++ main.rb
@@ -2,3 +2,14 @@ require 'dotenv/load'
 require 'tracker_api'
 
 client = TrackerApi::Client.new(token: ENV['API_TOKEN'])
+
+project = client.project(ENV['PROJECT_ID'])
+states = ['accepted', 'delivered', 'finished', 'started', 'rejected', 'planned', 'unstarted', 'unscheduled']
+
+stories = states.reduce({}) do |prev, cur|
+  cur = cur.to_sym
+  prev[cur] = project.stories(with_state: cur)
+  prev
+end
+
+puts stories.to_json
