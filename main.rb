dab935c60bc171fca40b81bf1ef9aecf57e1d785
Cody Swartz <ctsstc@gmail.com>
2017-08-04 17:46:52 -0700

    Checkpoint to test orders

--- main.rb
+++ main.rb
@@ -4,7 +4,16 @@ require 'tracker_api'
 client = TrackerApi::Client.new(token: ENV['API_TOKEN'])
 
 project = client.project(ENV['PROJECT_ID'])
-states = %i[accepted delivered finished started rejected planned unstarted unscheduled]
+#states = %i[accepted delivered finished started rejected planned unstarted unscheduled]
+states = %i[planned delivered finished started rejected]
+
+# aperently the come through in the order that they're displayed.
+all_stories = project.stories
+puts "SIZEN #{all_stories.count}"
+test = all_stories.map do |story|
+  [story.id, story.name, story.before_id, story.after_id]
+end
+puts test
 
 stories = states.each_with_object({}) do |state, memo|
   memo[state] = project.stories(with_state: state)
@@ -14,7 +23,7 @@ purdy = stories.map do |(key, stories)|
   puts ''
   puts "Key #{key} Stories #{stories.count}"
   { key => stories.map do |story|
-    puts "Story #{story.name}"
+    puts "Story #{story.name} #{story.id} "
     { story.name => story.labels.map(&:name) }
   end }
 end
