f9ecad3a5dc57e90f9f2dbd0b46efc25661f5cc7
Cody Swartz <ctsstc@gmail.com>
2017-08-07 19:57:33 -0700

    Code cleanup & tried to sort to no avail

--- main.rb
+++ main.rb
@@ -5,37 +5,40 @@ require 'tracker_api'
 client = TrackerApi::Client.new(token: ENV['API_TOKEN'])
 
 project = client.project(ENV['PROJECT_ID'])
-#states = %i[accepted delivered finished started rejected planned unstarted unscheduled]
+# states = %i[accepted delivered finished started rejected planned unstarted unscheduled]
 states = %i[planned delivered finished started rejected]
 
-# apparently they come through in the order that they're displayed.
-all_stories = project.stories
-puts "SIZEN #{all_stories.count}"
-test = all_stories.map do |story|
-  [story.id, story.name, story.before_id, story.after_id]
-end
-puts test
-
-stories = states.each_with_object({}) do |state, memo|
+types = states.each_with_object({}) do |state, memo|
   memo[state] = project.stories(with_state: state)
 end
 
 # Sort Collection By Labels
-#ar.sort do |a, b|
-#  case
-#  when a.x < b.x
-#    -1
-#  when a.x > b.x
-#    1
-#  else
-#    a.y <=> b.y
-#  end
-#end 
+types.each do |(_type_key, stories)|
+  stories.sort do |a, b|
+    al = a.labels
+    bl = b.labels
+    if al.count > 0 && bl.count > 0
+      if al[0].name < bl[0].name
+        -1
+      elsif al[0].name > bl[0].name
+        1
+      else
+        # a <=> b
+        0
+      end
+    # if b has labels and a doesn't prioritize b
+    elsif bl.count > 0
+      -1
+    else
+      0
+    end
+  end
+end
 
 # Update Order
 last_story = false
-stories.each do |(type_key, type)|
-  type.each do |story|
+types.each do |(_type_key, stories)|
+  stories.each do |story|
     if last_story
       story.after_id = last_story.id
       story.save
@@ -44,16 +47,3 @@ stories.each do |(type_key, type)|
     last_story = story
   end
 end
-
-purdy = stories.map do |(key, stories)|
-  puts ''
-  puts "Key #{key} Stories #{stories.count}"
-  { key => stories.map do |story|
-    puts "Story #{story.name} #{story.id} "
-    { story.name => story.labels.map(&:name) }
-  end }
-end
-
-puts ''
-puts 'PURDY'
-puts purdy.to_json
