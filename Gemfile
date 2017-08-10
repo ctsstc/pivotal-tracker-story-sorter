9375eb2fa39f60fb51eb77bf229ee28c5c5fe514
Cody Swartz <ctsstc@gmail.com>
2017-08-10 10:52:17 -0700

    Testing Label Fixes

--- Gemfile
+++ Gemfile
@@ -5,4 +5,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 gem 'dotenv-rails', groups: [:development, :test]
-gem 'tracker_api'
+gem 'tracker_api', git: 'https://github.com/dashofcode/tracker_api.git', branch: 'no-delete-labels-on-save'
--- main.rb
+++ main.rb
@@ -13,27 +13,27 @@ types = states.each_with_object({}) do |state, memo|
 end
 
 # Sort Collection By Labels
-types.each do |(_type_key, stories)|
-  stories.sort do |a, b|
-    al = a.labels
-    bl = b.labels
-    if al.count > 0 && bl.count > 0
-      if al[0].name < bl[0].name
-        -1
-      elsif al[0].name > bl[0].name
-        1
-      else
-        # a <=> b
-        0
-      end
-    # if b has labels and a doesn't prioritize b
-    elsif bl.count > 0
-      -1
-    else
-      0
-    end
-  end
-end
+#types.each do |(_type_key, stories)|
+#  stories.sort do |a, b|
+#    al = a.labels
+#    bl = b.labels
+#    if al.count > 0 && bl.count > 0
+#      if al[0].name < bl[0].name
+#        -1
+#      elsif al[0].name > bl[0].name
+#        1
+#      else
+#        # a <=> b
+#        0
+#      end
+#    # if b has labels and a doesn't prioritize b
+#    elsif bl.count > 0
+#      -1
+#    else
+#      0
+#    end
+#  end
+#end
 
 # Update Order
 last_story = false
